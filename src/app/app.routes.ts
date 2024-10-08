c0acafc2b809aeb51c06d585726e4e1a0777f32c
Jonathan-Buitrago <jonathanbuitrago505@gmail.com>
2024-10-08 12:08:59 -0500

    Add path alias for app folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from '../pages/dashboard/dashboard.component';
-import { HomeComponent } from '../pages/home/home.component';
+import { DashboardComponent } from '@pages/dashboard/dashboard.component';
+import { HomeComponent } from '@pages/home/home.component';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },

f24d77f41ec9a61649c7912efee19fa0f0988b53
Jonathan-Buitrago <jonathanbuitrago505@gmail.com>
2024-10-09 14:48:43 -0500

    Add login and signup pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from '@pages/dashboard/dashboard.component';
 import { HomeComponent } from '@pages/home/home.component';
 import { LoginComponent } from '@pages/login/login.component';
+import { SignupComponent } from '@pages/signup/signup.component';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
---
+++ src/pages/signup/signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AuthComponent } from '../../layouts/auth/auth.component';
+import { SignupFormComponent } from '../../components/signup-form/signup-form.component';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [AuthComponent, SignupFormComponent],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css',
+})
+export class SignupComponent {}

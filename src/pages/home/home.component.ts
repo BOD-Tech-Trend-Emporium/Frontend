8dc7f954a949b67b15fcd9f0b9d90908ed7bfbaf
Santiago Alvarez Ricardo <santiago.alvarez@endava.com>
2024-10-11 00:18:41 -0500

    create basic component

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { environment } from './../../environments/environment';
+import { NavbarComponent } from '@components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })

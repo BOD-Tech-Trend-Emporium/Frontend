6597a5746032ffdc8711ef00ab3f76cd11954e80
Jhoan Oswaldo Ome Vega <Jhoan-Oswaldo.Ome@endava.com>
2024-10-17 10:59:39 -0500

    Add image into product-detail component

--- src/pages/product-detail/product-detail.component.ts
+++ src/pages/product-detail/product-detail.component.ts
@@ -1,23 +1,43 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { ProductEntity } from '@entities/Product.entity';
 import { ProductService } from '@services/product/product.service';
 import { ToastrService } from 'ngx-toastr';
+import { BasicImagenComponent } from "../../components/basic-imagen/basic-imagen.component";
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [BasicImagenComponent],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent implements OnInit{
   private productService: ProductService = inject(ProductService);
+  private route = inject(ActivatedRoute);
   productEntity?: ProductEntity;
   isLoading = true;
   toastr: ToastrService = inject(ToastrService);
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.route.params.subscribe({
+      next:(params: Params)=>{
+        this.productService.getProductById(params['id']).subscribe({
+          next:(data: ProductEntity)=>{
+            this.productEntity = data;
+            this.isLoading = false;
+          },
+          error:(error:any)=>{
+            error.error.Message ? this.toastr.error(error.error.Message) : this.toastr.error('Unexpected error occurred') 
+            this.isLoading = false;
+          }
+        })
+      },
+      error:(error:any)=>{
+        this.toastr.error('Unexpected error occurred');
+      }
+
+    })
   }
 
 }

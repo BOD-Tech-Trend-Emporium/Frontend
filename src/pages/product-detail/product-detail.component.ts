adfaffebcab22b89f38196749add9230f80f79be
Jhoan Oswaldo Ome Vega <Jhoan-Oswaldo.Ome@endava.com>
2024-10-17 21:46:03 -0500

    Add basic-image and productdetail-description components inside of product detail

--- src/pages/product-detail/product-detail.component.ts
+++ src/pages/product-detail/product-detail.component.ts
@@ -1,21 +1,23 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { ProductEntity } from '@entities/Product.entity';
+import { ProductByIdDto, ProductEntity } from '@entities/Product.entity';
 import { ProductService } from '@services/product/product.service';
 import { ToastrService } from 'ngx-toastr';
 import { BasicImagenComponent } from "../../components/basic-imagen/basic-imagen.component";
+import { ProductdetailDescriptionComponent } from '@components/productdetail-description/productdetail-description.component';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [BasicImagenComponent],
+  imports: [BasicImagenComponent, ProductdetailDescriptionComponent],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent implements OnInit{
+
   private productService: ProductService = inject(ProductService);
   private route = inject(ActivatedRoute);
-  productEntity?: ProductEntity;
+  productEntity?: ProductByIdDto;
   isLoading = true;
   toastr: ToastrService = inject(ToastrService);
 
@@ -23,7 +25,7 @@ export class ProductDetailComponent implements OnInit{
     this.route.params.subscribe({
       next:(params: Params)=>{
         this.productService.getProductById(params['id']).subscribe({
-          next:(data: ProductEntity)=>{
+          next:(data: ProductByIdDto)=>{
             this.productEntity = data;
             this.isLoading = false;
           },
@@ -39,5 +41,6 @@ export class ProductDetailComponent implements OnInit{
 
     })
   }
+
 
 }

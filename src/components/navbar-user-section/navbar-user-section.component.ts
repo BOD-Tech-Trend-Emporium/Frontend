f58ae285ef23d08ef92b242640654b67960ea75e
Santiago Alvarez Ricardo <santiago.alvarez@endava.com>
2024-10-22 01:07:20 -0500

    remains connect to log-in service

--- src/components/navbar-user-section/navbar-user-section.component.ts
+++ src/components/navbar-user-section/navbar-user-section.component.ts
@@ -18,9 +18,6 @@ import { environment } from 'src/environments/environment.local';
   styleUrl: './navbar-user-section.component.css'
 })
 export class NavbarUserSectionComponent {
-  authService: AuthService = inject(AuthService);
-  toastr: ToastrService = inject(ToastrService);
-  router: Router = inject(Router);
 
   user: UserEntity= {
     email : "Example@Email",
@@ -30,20 +27,4 @@ export class NavbarUserSectionComponent {
     createdAt:'2024-05-17',
     name: 'Diego'
   }
-
-
-
-  async handleLogout() {
-    const response: any = await this.authService.logout();
-    if (response.data) {
-      this.toastr.success('Logout successfully');
-    } else {
-      this.toastr.error('Somethign went wrong, try again');
-    }
-    setTimeout(() => {
-      this.router.navigate(['/']);
-    }, 500);
-  }
-
-
 }
